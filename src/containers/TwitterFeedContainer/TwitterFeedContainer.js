8fb48e20b72477ffde61ddd7b6f92283bb2945ed
Cassandra Wilcox <csndra0504@gmail.com>
2016-11-14 14:14:25 -0500

    chore: bootstrap TwitterFeedContainer

---
+++ src/containers/TwitterFeedContainer/TwitterFeedContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TwitterFeedContainer extends Component {
+  render () {
+    return (
+        <p>Twitter Feed Container</p>
+      );
+  }
+};
+
+export default TwitterFeedContainer;
