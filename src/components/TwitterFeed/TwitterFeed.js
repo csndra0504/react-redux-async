8f1570498a7e0c93210821dd5c19e3deac255032
Cassandra Wilcox <csndra0504@gmail.com>
2016-11-14 19:35:09 -0500

    feat: display feed

--- src/components/TwitterFeed/TwitterFeed.js
+++ src/components/TwitterFeed/TwitterFeed.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 
 class TwitterFeed extends Component {
   render () {
+    const {feed} = this.props
+    console.log('TwitterFeed items',feed)
     return (
-        <p>Twitter Feed Component</p>
+        <div>
+          {feed.map(item => <p key={item}>{item}</p>)}
+        </div>
       );
   }
 };
