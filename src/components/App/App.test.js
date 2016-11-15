8b85db7cb37958ab5d8ef9eaf3b379cd67e3f2ea
Cassandra Wilcox <csndra0504@gmail.com>
2016-11-14 19:56:45 -0500

    test: pass render without crashing test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from '../../reducers/index';
 import App from './App';
+
+const middleware = [ thunk ];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider> , div);
 });
