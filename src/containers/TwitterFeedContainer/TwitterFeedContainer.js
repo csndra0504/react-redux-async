0b00fb42095a003ca73a5272df76405aae8e9d9c
Cassandra Wilcox <csndra0504@gmail.com>
2016-11-14 19:34:52 -0500

    feat: fetch feed

--- src/containers/TwitterFeedContainer/TwitterFeedContainer.js
+++ src/containers/TwitterFeedContainer/TwitterFeedContainer.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import TwitterSearch from '../../components/TwitterSearch/TwitterSearch';
 import TwitterFeed from '../../components/TwitterFeed/TwitterFeed';
+import {fetchTweets} from '../../actions/index.js'
 
 class TwitterFeedContainer extends Component {
+  
+  componentDidMount() {
+    const { dispatch, query } = this.props
+    dispatch(fetchTweets(query))
+  }
+
   render () {
+
+    const {items} = this.props
+
     return (
       <div>
-        <p>Twitter Feed Container</p>
+        <p>Feed Container</p>
         <TwitterSearch />
-        <TwitterFeed />
+        <TwitterFeed feed={items}/>
       </div>
     );
   }
 };
 
-export default TwitterFeedContainer;
+const mapStateToProps = state => {
+  console.log('TwitterFeedContainer state', state)
+  const { query, tweets } = state;
+  const {
+    isFetching,
+    lastUpdated,
+    items
+  } = tweets || {
+    isFetching: true,
+    items: []
+  }
+
+  return {
+    query,
+    items,
+    isFetching,
+    lastUpdated
+  }
+}
+
+export default connect(mapStateToProps)(TwitterFeedContainer);
